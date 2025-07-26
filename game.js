19a7935c078cd7dfd52329aca65c3c097fd87dc3
czblol <qianbin261115@gmail.com>
2025-07-26 17:41:59 +0800

    Added vertical shop layout with fruits

--- game.js
+++ game.js
@@ -42,4 +42,44 @@ function plantSeed(index) {
   }
 }
 
+let coins = 50;
+let seeds = {
+  apple: 0,
+  banana: 0,
+  orange: 0
+};
+
+function updateStats() {
+  document.getElementById('coins').innerText = coins;
+  // 如果你有显示各个种子的数量，也更新它们
+}
+
+function buyApple() {
+  if (coins >= 10) {
+    coins -= 10;
+    seeds.apple++;
+    updateStats();
+  } else {
+    alert("Not enough coins!");
+  }
+}
+function buyBanana() {
+  if (coins >= 15) {
+    coins -= 15;
+    seeds.banana++;
+    updateStats();
+  } else {
+    alert("Not enough coins!");
+  }
+}
+function buyOrange() {
+  if (coins >= 20) {
+    coins -= 20;
+    seeds.orange++;
+    updateStats();
+  } else {
+    alert("Poor Guy Not enough coins!");
+  }
+}
+
 updateUI();
